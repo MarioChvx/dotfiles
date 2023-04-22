54bb1327dfb653c6d3f7fbeba97e4ac282a739bd
Mario Chavez <mariochavez1292@gmail.com>
2023-04-22 12:21:34 -0600

    lsp languages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,14 @@
 
 
 if vim.g.vscode then
-    	-- VSCode extension
+    -- VSCode extension
 else
-	-- ordinary Neovim
-	require('mario.keymaps')
-	require('mario.packer')
-	require('mario.plugins_conf')
-	require('mario.colors')
-	require('mario.keymaps')
+    -- ordinary Neovim
+    require('mario.keymaps')
+    require('mario.packer')
+    require('mario.plugins_conf')
+    require('mario.colors')
+    require('mario.keymaps')
 end
 
 require('mario.vanilla_conf')
