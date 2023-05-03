34d0ac6503c90135ac96080a17a352f2d2db5ce0
Mario Chavez <mariochavez1292@gmail.com>
2023-05-03 16:59:54 -0600

    keybinds

--- .config/nvim/lua/mario/plugins_conf/toggleterm.lua
+++ .config/nvim/lua/mario/plugins_conf/toggleterm.lua
@@ -3,12 +3,12 @@ require("toggleterm").setup{
 }
 
 -- KEYMAPS
-vim.keymap.set("n", "<leader>cc", vim.cmd.ToggleTerm)
+vim.keymap.set("n", "<C-t>", vim.cmd.ToggleTerm)
 
 -- TERM MODE
 function _G.set_terminal_keymaps()
     local opts = {buffer = 0}
-    vim.keymap.set('t', '<C-w>', vim.cmd.ToggleTerm, opts)
+    vim.keymap.set('t', '<C-t>', vim.cmd.ToggleTerm, opts)
 end
 
 vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
